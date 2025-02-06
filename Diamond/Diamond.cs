b49bfe2d9d378269cf4cb9af98a8536145201d4b
Krzysztof Wróbel <k.wrobel@reply.com>
2025-02-06 23:12:29 +0100

    We have correct outer spacing, green (fixed test)

--- Diamond/Diamond.cs
+++ Diamond/Diamond.cs
@@ -7,14 +7,31 @@ public sealed class Diamond
     {
         // We are calculating the distance by considering ASCII codes integer representation
         var characterDistance = input - StartingCharacter;
-
+        // number of spaces around character are equal the distance from 'A'
+        // we can calculate row length by multiplying distance by 2 and adding 1 to count the character
+        var totalRowLength = characterDistance * 2 + 1;
         var diamondRowsCollection = new List<DiamondRow>();
         // We are moving around the middle of the row 
         for (var i = -characterDistance; i <= characterDistance; i++)
         {
-            diamondRowsCollection.Add(new DiamondRow() { Character = 'A', InnerSpacesCount = 0, OuterSpacesCount = 0});
+            diamondRowsCollection.Add(CreateDiamondRow(i, characterDistance, totalRowLength));
         }
 
-        return diamondRowsCollection;
+        return diamondRowsCollection.OrderBy(p=>p.SortOrder).ToList();;
+    }
+
+    private static DiamondRow CreateDiamondRow(int currentLineIndex, int characterDistance, int totalRowLength)
+    {
+        // we are using this index to position rows in the list to allow easy printout
+        var newRow = new DiamondRow() { SortOrder = currentLineIndex };
+        
+        // using Math.Abs to always have positive number, even on negative sort order index
+        var outerSpacesCount = Math.Abs(currentLineIndex); 
+        // we know the number of outer spaces now
+        newRow.OuterSpacesCount = outerSpacesCount;
+        // calculate the actual character for this row
+        newRow.Character = (char)(StartingCharacter + characterDistance - outerSpacesCount);
+
+        return newRow;
     }
 }
