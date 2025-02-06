35878ace6fc2b485283d6dc529ae816ddaeb8c7a
Krzysztof Wróbel <k.wrobel@reply.com>
2025-02-06 21:53:56 +0100

    First test, starting with red

---
+++ DIamond.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace DIamond.Tests;
+
+public class UnitTest1
+{
+    // Start with the simplest happy path, build diamond with letter A for input A
+    [Fact]
+    public void Given_validInput_A_BuildDiamondWithA()
+    {
+        var result = Diamond.Build('A');
+        Assert.Equal("A", result);
+    }
+}
+
+public sealed class Diamond
+{
+    public static string Build(char input)
+    {
+        throw new NotImplementedException();
+    }
+}
