ded7862f576339ab9aa1145158ff17ca19b936d5
Krzysztof Wróbel <k.wrobel@reply.com>
2025-02-06 23:50:51 +0100

    Raw working Kata, ready for improvements

--- Diamond/Program.cs
+++ Diamond/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Diamond;
 
-Console.WriteLine("Hello, World!");
+if (args.Length != 1 ||
+    args[0].Length != 1)
+{
+    Console.WriteLine("Usage: dotnet Diamond.dll [Uppercase character]");
+    return;
+}
+
+var inputChar = args[0][0];
+
+var printer = new DiamondPrinter();
+printer.Print(inputChar);
