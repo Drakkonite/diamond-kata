c5fb38f190f00c94c0071a9589cb21b34202254c
Krzysztof Wróbel <k.wrobel@reply.com>
2025-02-06 22:16:32 +0100

    Exapnd the structure with objects, red

--- DIamond.Tests/DiamondTests.cs
+++ DIamond.Tests/DiamondTests.cs
@@ -2,11 +2,37 @@ namespace Diamond.Tests;
 
 public class DiamondTests
 {
-    // Start with the simplest happy path, build diamond with letter A for input A
+    /*
+     * I need to represent diamond with some structure to allow testing and avoid
+     * procedural flow with several for loops printing strings
+     *
+     * First return a row of diamond for input A
+     * It consists of:
+     * - preceeding and ending spaces count (it's always the same value for every row)
+     * - row length it's represented by distance from INPUT char to 'A'
+     * - inner spaces count - represents the number of spaces between characters in a row
+     */
     [Fact]
-    public void Given_validInput_A_BuildDiamondWithA()
+    public void Given_validInput_A_BuildDiamond_returns_a_single_row_with_A()
     {
-        var result = Diamond.Build('A');
-        Assert.Equal("A", result);
+        const char input = 'A';
+        var middlePointRow = new DiamondRow()
+        {
+            OuterSpacesCount = 0,
+            Character = 'A',
+            InnerSpacesCount = 0
+        };
+        
+        var result = Diamond.Build(input);
+        
+        Assert.Equal(middlePointRow, result);
     }
+    
+}
+
+public sealed class DiamondRow
+{
+    public int OuterSpacesCount { get; set; }
+    public char Character { get; set; }
+    public int InnerSpacesCount { get; set; }
 }
